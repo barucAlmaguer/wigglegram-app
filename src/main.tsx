9c9b4a3023dcec17e22e4ab834fc098783a01c33
Baruc Almaguer <baruc.almaguer@gmail.com>
2025-10-23 10:38:28 -0600

    Inject Google Analytics in production builds

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,33 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+type GtagArgs = [string, ...unknown[]]
+
+declare global {
+  interface Window {
+    dataLayer: unknown[]
+    gtag?: (...args: GtagArgs) => void
+  }
+}
+
+if (import.meta.env.PROD) {
+  // Only inject Google Analytics in production to keep local dev traffic out of reports.
+  window.dataLayer = window.dataLayer || []
+
+  const script = document.createElement('script')
+  script.async = true
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-00C5P1KHS4'
+  document.head?.appendChild(script)
+
+  const gtag = (...args: GtagArgs) => {
+    window.dataLayer.push(args)
+  }
+  window.gtag = gtag
+
+  gtag('js', new Date())
+  gtag('config', 'G-00C5P1KHS4')
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
